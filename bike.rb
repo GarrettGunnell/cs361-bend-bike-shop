e5d6a8acf4815569baa6cc55b24819197c04a25f
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-02-10 10:39:51 -0800

    Store cargo in CargoContainer class rather than Bike class

--- bike.rb
+++ bike.rb
@@ -3,9 +3,8 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rented
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
@@ -13,11 +12,21 @@ class Bike
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
   end
 
   def rent!
     self.rented = true
+  end
+end
+
+class CargoContainer
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize()
+    @cargo_contents = []
   end
 
   def add_cargo(item)
@@ -35,5 +44,4 @@ class Bike
   def pannier_remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
-
 end
